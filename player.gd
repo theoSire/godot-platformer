149f184431870627465242230aced18e677f9748
Theofrid <theofridthiyadore@gmail.com>
2024-01-11 22:49:37 -0500

    - Created an air_jump variable to determine whether the player jumped   previously - Modified the handle_jump function for double jumping - Added functions to handle the air acceleration and wall jump

--- player.gd
+++ player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var movement_data : PlayerMovementData
 
+var air_jump = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -10,9 +12,11 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	apply_gravity(delta)
+	handle_wall_jump()
 	handle_jump()
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	handle_acceleration(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
 	update_animations(input_axis)
@@ -30,17 +34,40 @@ func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta
 
+func handle_wall_jump():
+	if not is_on_wall(): return
+	var wall_normal = get_wall_normal()
+	print(wall_normal == Vector2.LEFT)
+	if Input.is_action_just_pressed("ui_left") and wall_normal == Vector2.LEFT:
+		velocity.x = wall_normal.x * movement_data.speed * 2.0
+		velocity.y = movement_data.jump_velocity
+	elif Input.is_action_just_pressed("ui_right") and wall_normal == Vector2.RIGHT:
+		velocity.x = wall_normal.x * movement_data.speed
+		velocity.y = movement_data.jump_velocity
+
 func handle_jump():
+	if is_on_floor(): air_jump = true
+
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = movement_data.jump_velocity
+			velocity.y = movement_data.jump_velocity		 
 	if not is_on_floor():
 		if Input.is_action_just_released("ui_up") && velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
-
+			
+		if Input.is_action_just_pressed("ui_up") and air_jump:
+			velocity.y = movement_data.jump_velocity * 0.8
+			air_jump = false
+ 
 func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
 	if input_axis:
 		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta)
 
 func apply_friction(input_axis, delta):
 	if input_axis == 0 and is_on_floor():
