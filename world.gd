048375b6f3f427ea6f66719db9ff2fd192d8c480
Theofrid <theofridthiyadore@gmail.com>
2024-01-15 00:22:01 -0500

    World.tscn - The LevelCompleted scene was added to the World scene within a   CanvasLayer since it's a UI node (part of the Control node) and to   avoid conflict with the main World (Node) node
    world.gd
    - Created a variable to access the LevelCompleted child node
    - Connected the level_completed signal with the show_level_completed()
      function and makes the LevelCompleted node visible and pauses the
      game

--- world.gd
+++ world.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 @onready var collision_polygon_2D = $StaticBody2D/CollisionPolygon2D
 @onready var polygon_2D = $StaticBody2D/CollisionPolygon2D/Polygon2D
-
+@onready var level_completed = $CanvasLayer/LevelCompleted
 func _ready():
 	RenderingServer.set_default_clear_color(Color.FIREBRICK)
 	polygon_2D.polygon = collision_polygon_2D.polygon
+	Events.level_completed.connect(show_level_completed)
+
+func show_level_completed():
+	level_completed.show()
+	get_tree().paused = true
