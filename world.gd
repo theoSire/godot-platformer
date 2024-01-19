71df68909f5d407d325d26795c050cec0e8f2598
Theofrid <theofridthiyadore@gmail.com>
2024-01-19 01:48:27 -0500

    world.gd - Added code for transition effects between each levels and after the   last level
    Other files
    - Tweaked some values for the player movement and safe margin

--- world.gd
+++ world.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var next_level: PackedScene
+
 @onready var level_completed = $CanvasLayer/LevelCompleted
 func _ready():
 	RenderingServer.set_default_clear_color(Color.FIREBRICK)
@@ -8,3 +10,8 @@ func _ready():
 func show_level_completed():
 	level_completed.show()
 	get_tree().paused = true
+	await LevelTransition.fade_to_black()
+	if not next_level is PackedScene: return
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
+	LevelTransition.fade_from_black()
