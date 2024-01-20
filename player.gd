84f872a385f7cb5bc2271e816b60057a51b28acb
Theofrid <theofridthiyadore@gmail.com>
2024-01-20 14:52:28 -0500

    project.godot - Added new Input Maps for moving left and right, jumping, and slower   and faster player movements
    player.gd
    - Changed the built-in keys with the new Input maps
    - Fixed the player wall collision jitter by changing return if statement
      from "not is_on_wall()" with "not is_on_wall_only()"

--- player.gd
+++ player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta):
 	apply_gravity(delta)
 	handle_wall_jump()
 	handle_jump()
-	var input_axis = Input.get_axis("ui_left", "ui_right")
+	var input_axis = Input.get_axis("move_left", "move_right")
 	handle_acceleration(input_axis, delta)
 	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
@@ -26,7 +26,7 @@ func _physics_process(delta):
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0 #after moving
 	if just_left_ledge:
 		coyote_jump_timer.start()
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("fast_movement"):
 		movement_data = load("res://FasterMovementData.tres")
 	if Input.is_action_just_pressed("slow_movement"):
 		movement_data = load("res://SlowMovementData.tres")
@@ -36,14 +36,14 @@ func apply_gravity(delta):
 		velocity.y += gravity * movement_data.gravity_scale * delta
 
 func handle_wall_jump():
-	if not is_on_wall(): return
+	if not is_on_wall_only(): return
 	var wall_normal = get_wall_normal()
 	var left_angle = abs(wall_normal.angle_to(Vector2.LEFT))
 	var right_angle = abs(wall_normal.angle_to(Vector2.RIGHT))
-	if Input.is_action_just_pressed("ui_left") and (wall_normal.is_equal_approx(Vector2.LEFT) or left_angle < 10.0):
+	if Input.is_action_just_pressed("move_left") and (wall_normal.is_equal_approx(Vector2.LEFT) or left_angle < 10.0):
 		velocity.x = wall_normal.x * movement_data.speed * 2.0
 		velocity.y = movement_data.jump_velocity
-	elif Input.is_action_just_pressed("ui_right") and (wall_normal.is_equal_approx(Vector2.RIGHT) or right_angle < 10.0):
+	elif Input.is_action_just_pressed("move_right") and (wall_normal.is_equal_approx(Vector2.RIGHT) or right_angle < 10.0):
 		velocity.x = wall_normal.x * movement_data.speed
 		velocity.y = movement_data.jump_velocity
 
@@ -51,15 +51,15 @@ func handle_jump():
 	if is_on_floor(): air_jump = true
 
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("jump"):
 			velocity.y = movement_data.jump_velocity		 
 	elif not is_on_floor():
 		#double jump (air jump)
-		if Input.is_action_just_released("ui_up") && velocity.y < movement_data.jump_velocity / 2:
+		if Input.is_action_just_released("jump") && velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
 		
 		#avoid air jump near walls
-		if Input.is_action_just_pressed("ui_up") and air_jump and not is_on_wall():
+		if Input.is_action_just_pressed("jump") and air_jump and not is_on_wall():
 			velocity.y = movement_data.jump_velocity * 0.8
 			air_jump = false
  
