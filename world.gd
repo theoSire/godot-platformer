9d6c1168b3e089a7f3e2c559d2200ab8ea1e68a1
Theofrid <theofridthiyadore@gmail.com>
2024-01-25 21:33:49 -0500

    level_completed.tscn & level_completed.gd - Added new buttons for the retrying the level and going to the next   level (positioned it with the center container, vbox and hbox   vcontainers) - Added a script in which signals are emitted by these buttons are   pressed
    world.tscn & world.gd
    - Added a level timer to record the time taken by the player to finish
      the level
    - Connected the previously created signals to the associated functions
    -

--- world.gd
+++ world.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @export var next_level: PackedScene
 
+var level_time = 0.0
+var start_level_msec = 0.0
+
+@onready var level_time_label = %LevelTimeLabel
 @onready var level_completed = $CanvasLayer/LevelCompleted
 @onready var start_in = %StartIn
 @onready var start_in_label = %CanvasLayer/StartIn/CenterContainer/StartInLabel
@@ -17,11 +21,30 @@ func _ready():
 	await animation_player.animation_finished
 	start_in.visible = false
 	get_tree().paused = false
+	start_level_msec = Time.get_ticks_msec()
 
-func show_level_completed():
-	get_tree().paused = true
-	level_completed.show()
+func _process(delta):
+	level_time = (Time.get_ticks_msec() - start_level_msec)
+	level_time_label.text = str(level_time / 1000.0)
+
+func retry():
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_file(scene_file_path)
+
+func go_to_next_level():
 	if not next_level is PackedScene: return
 	await LevelTransition.fade_to_black()
 	get_tree().change_scene_to_packed(next_level)
 	LevelTransition.fade_from_black()
+
+func show_level_completed():
+	level_completed.show()
+	level_completed.retry_button.grab_focus()
+	get_tree().paused = true
+
+func _on_level_completed_next_level():
+	go_to_next_level()
+
+func _on_level_completed_retry():
+	retry()
